2ba93f8744d5b0358c8d00f8276d87e15fa7906f
Garrison <garrison@pc103895.PKRVMN.local>
2023-06-19 15:27:19 +0300

    доработан компонент кнопкиЭ

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,24 @@ import { HiveButton } from '.';
 </script>
 
 <template>
-  <hive-button>Click</hive-button>
+  <hive-button>
+    <img src="@/assets/search.svg" alt="image after" class="img" />
+    Click
+     <img src="@/assets/search.svg" alt="image before" class="img" />
+  </hive-button>
+
+  <hive-button disabled />
+
+  <hive-button title="Classes" :class="'test'" />
 </template>
+
+<style lang="scss" scoped>
+.img {
+  width: 15px;
+  height: 15px;
+}
+
+.test {
+  background-color: azure;
+}
+</style>
