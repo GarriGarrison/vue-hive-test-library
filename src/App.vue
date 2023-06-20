88e3c3ce07baf4217b946788eeceb37e18ae2446
Garrison <garrison@pc103895.PKRVMN.local>
2023-06-20 12:28:48 +0300

    добавлена обёртка в App.vue для удобного просмотра компонентов

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,45 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import { HiveButton, HiveLoader } from '.';
+
+const text = ref('text');
 </script>
 
 <template>
-  <hive-button>
-    <img src="@/assets/search.svg" alt="image after" class="img" />
-    Click
-    <img src="@/assets/search.svg" alt="image before" class="img" />
-  </hive-button>
+  <hive-loader />
 
-  <hive-button disabled />
+  <div class="wrapper">
+    <!-- Button -->
+    <widget-wrapper title="Button">
+      <hive-button>
+        <img src="@/assets/search.svg" alt="image after" class="img" />
+        Click
+        <img src="@/assets/search.svg" alt="image before" class="img" />
+      </hive-button>
 
-  <hive-button :style="{backgroundColor: 'red'}" />
+      <hive-button disabled />
 
-  <hive-button title="Classes" :class="'test'" />
+      <hive-button :style="{ backgroundColor: 'red' }" />
 
-  <hive-loader visible />
+      <hive-button title="Classes" :class="'test'" />
+    </widget-wrapper>
+
+     <!-- Textarea -->
+    <widget-wrapper title="Textarea">
+      {{ text }}
+      <hive-textarea v-model="text" resize-direction="both" />
+    </widget-wrapper>
+  </div>
 </template>
 
 <style lang="scss" scoped>
+
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  min-width: 400px;
+}
 .img {
   width: 15px;
   height: 15px;
