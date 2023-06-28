298174087a76edf8c657e06c6b0b2e4665c968e8
Garrison <garrison@pc103895.PKRVMN.local>
2023-06-28 15:41:52 +0300

    сделан компонент Dialog

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import HiveButton from '@/components/hive-button/hive-button.vue';
 import HiveInput from '@/components/hive-input/hive-input.vue';
 import HiveLoader from '@/components/hive-loader/hive-loader.vue';
+import HiveDialog from '@/components/hive-dialog/hive-dialog.vue'
 import HiveTextarea from '@/components/hive-textarea/hive-textarea.vue';
 
-export { HiveButton, HiveInput, HiveLoader, HiveTextarea };
+export { HiveButton, HiveInput, HiveLoader, HiveDialog, HiveTextarea };
